07a7ed9759fd49961aeec68d2562102091d5b508
Eshan Bhatt <eshan18.09@gmail.com>
2021-01-26 12:32:40 +0530

    added questions

---
+++ Recursion and Backtracking/abbreviationUsingBacktracking.java
@@ -0,0 +1,34 @@
+/*
+1. You are given a word.
+2. You have to generate all abbrevations of that word.
+*/
+
+import java.util.*;
+
+public class abbreviationUsingBacktracking {
+
+    public static void solution(String str, String asf, int count, int pos) {
+        if (pos == str.length()) {
+            if (count == 0) {
+                System.out.println(asf);
+            } else {
+                System.out.println(asf + count);
+            }
+            return;
+        }
+
+        if (count != 0) {
+            solution(str, asf + count + str.charAt(pos), 0, pos + 1);
+        } else {
+            solution(str, asf + str.charAt(pos), 0, pos + 1);
+        }
+        solution(str, asf, count + 1, pos + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        solution(str, "", 0, 0);
+        scn.close();
+    }
+}
