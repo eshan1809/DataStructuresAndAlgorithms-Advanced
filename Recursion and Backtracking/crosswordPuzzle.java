94658e8c37e8fff4af5e3900a771b19dfc12d97c
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-01 12:43:13 +0530

    added more questions

--- Recursion and Backtracking/crosswordPuzzle.java
+++ Recursion and Backtracking/crosswordPuzzle.java
@@ -1,3 +1,9 @@
+/*
+1. You are given a 10*10 2-D array(arr) containing only '+' and '-' characters, which represents a 
+    crossword puzzle. 
+2. You are also given n number of words which need to be filled into the crossword.
+3. Cells containing '-' are to be filled with the given words.
+*/
 
 import java.util.*;
 
