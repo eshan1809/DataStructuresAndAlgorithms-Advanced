594d374057c0efaa5d536d43acafaee7ac7e5666
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-03 14:27:27 +0530

    added more questions

--- Recursion and Backtracking/FriendsPairing.java
+++ Recursion and Backtracking/FriendsPairing.java
@@ -1,3 +1,9 @@
+/*
+1. You are given an integer n, which represents n friends numbered from 1 to n.
+2. Each one can remain single or can pair up with some other friend.
+3. You have to print all the configurations in which friends can remain single or can be paired up.
+*/
+
 import java.util.*;
 
 public class FriendsPairing {
