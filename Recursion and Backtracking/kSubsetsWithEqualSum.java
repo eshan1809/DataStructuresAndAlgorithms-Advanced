a4b30418c445543f022a938c3776c283c4c73fbd
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-07 12:40:17 +0530

    added more questions

--- Recursion and Backtracking/kSubsetsWithEqualSum.java
+++ Recursion and Backtracking/kSubsetsWithEqualSum.java
@@ -1,3 +1,9 @@
+/*
+1. You are given an array of n distinct integers.
+2. You have to divide these n integers into k non-empty subsets such that sum of integers of every 
+     subset is same.
+3. If it is not possible to divide, then print "-1".
+*/
 
 import java.util.*;
 
