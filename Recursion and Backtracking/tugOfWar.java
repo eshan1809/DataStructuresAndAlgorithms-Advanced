0f0a969c20f86a40a5ea571da41ce3548bcf0410
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-08 11:35:30 +0530

    added more questions

---
+++ Recursion and Backtracking/tugOfWar.java
@@ -0,0 +1,7 @@
+/*
+ * 1. You are given an array of n integers. 2. You have to divide these n
+ * integers into 2 subsets such that the difference of sum of two subsets is as
+ * minimum as possible. 3. If n is even, both set will contain exactly n/2
+ * elements. If is odd, one set will contain (n-1)/2 and other set will contain
+ * (n+1)/2 elements. 3. If it is not possible to divide, then print "-1".
+ */
