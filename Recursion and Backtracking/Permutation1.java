948956b4881742be44f6418d9930f0f3b2f9945c
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-13 19:39:32 +0530

    added more questions

---
+++ Recursion and Backtracking/Permutation1.java
@@ -0,0 +1,5 @@
+/*
+ * 1. You are give a number of boxes (nboxes) and number of non-identical items
+ * (ritems). 2. You are required to place the items in those boxes and print all
+ * such configurations possible.
+ */
