c905fb3c4036355375eef488ad9f3a497327c743
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-13 19:40:26 +0530

    added more questions

--- Recursion and Backtracking/Permutation1.java
+++ Recursion and Backtracking/Permutation1.java
@@ -3,3 +3,34 @@
  * (ritems). 2. You are required to place the items in those boxes and print all
  * such configurations possible.
  */
+
+import java.io.*;
+import java.util.*;
+
+public class Permutation1 {
+
+    public static void permutations(int[] boxes, int ci, int ti) {
+        if (ci > ti) {
+            for (int i : boxes) {
+                System.out.print(i);
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < boxes.length; i++) {
+            if (boxes[i] == 0) {
+                boxes[i] = ci;
+                permutations(boxes, ci + 1, ti);
+                boxes[i] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int nboxes = Integer.parseInt(br.readLine());
+        int ritems = Integer.parseInt(br.readLine());
+        permutations(new int[nboxes], 1, ritems);
+    }
+
+}
