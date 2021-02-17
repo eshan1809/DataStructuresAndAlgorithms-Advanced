bc5ffaba4f249bc379fbf80e4113aad7212a8827
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-17 15:55:25 +0530

    added more questions

--- Recursion and Backtracking/Queens_Combinations_2dAs2d_BoxChooses.java
+++ Recursion and Backtracking/Queens_Combinations_2dAs2d_BoxChooses.java
@@ -1,3 +1,8 @@
+/*
+1. You are given a number n, representing the size of a n * n chess board.
+2. You are required to calculate and print the combinations in which n queens can be placed on the n * n chess-board.
+*/
+
 import java.io.*;
 import java.util.*;
 
