0693b6ff48cc7cbbd06a25504a8e6e50df64d082
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-17 15:55:01 +0530

    added more questions

--- Recursion and Backtracking/Combination2.java
+++ Recursion and Backtracking/Combination2.java
@@ -5,7 +5,6 @@
  */
 
 import java.io.*;
-import java.util.*;
 
 public class Combination2 {
 
---
+++ Recursion and Backtracking/Queens_Combinations_2dAs2d_BoxChooses.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Queens_Combinations_2dAs2d_BoxChooses {
+    public static void queensCombinations(int qpsf, int tq, int row, int col, String asf) {
+        // write your code here
+        if (row == tq) {
+            if (qpsf == tq) {
+                System.out.print(asf);
+                System.out.println();
+            }
+            return;
+        }
+
+        if (col < tq - 1) {
+            queensCombinations(qpsf + 1, tq, row, col + 1, asf + "q");
+            queensCombinations(qpsf, tq, row, col + 1, asf + "-");
+        } else {
+            queensCombinations(qpsf + 1, tq, row + 1, 0, asf + "q");
+            queensCombinations(qpsf, tq, row + 1, 0, asf + "-");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        queensCombinations(0, n, 0, 0, "");
+    }
+}
