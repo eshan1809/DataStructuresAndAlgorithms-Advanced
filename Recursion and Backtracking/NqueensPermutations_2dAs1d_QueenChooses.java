784739c9a3a6abd7a321bbdb4de054ad1bc73ad8
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-24 11:08:02 +0530

    added more questions

---
+++ Recursion and Backtracking/NqueensPermutations_2dAs1d_QueenChooses.java
@@ -0,0 +1,9 @@
+/*
+1. You are given a number n, representing the size of a n * n chess board.
+2. You are required to calculate and print the permutations in which n queens (distinct) can be placed on the n * n chess-board. 
+3. No queen shall be able to kill another.
+*/
+
+public class NqueensPermutations_2dAs1d_QueenChooses {
+
+}
