8e52cce7a45bddaa6c7875dd725035e8026caf31
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-27 00:56:00 +0530

    added more questions

---
+++ Recursion and Backtracking/Permutations_Words_2.java
@@ -0,0 +1,8 @@
+/*
+1. You are given a word (may have one character repeat more than once).
+2. You are required to generate and print all arrangements of these characters. 
+*/
+
+public class Permutations_Words_2 {
+
+}
