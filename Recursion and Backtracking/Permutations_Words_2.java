52c1c869df407dde8b996c3862a1eaf5041e11da
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-27 00:58:35 +0530

    added more questions

--- Recursion and Backtracking/Permutations_Words_2.java
+++ Recursion and Backtracking/Permutations_Words_2.java
@@ -3,6 +3,41 @@
 2. You are required to generate and print all arrangements of these characters. 
 */
 
+import java.io.*;
+import java.util.*;
+
 public class Permutations_Words_2 {
 
+    public static void generateWords(int cc, String str, Character[] spots, HashMap<Character, Integer> lastOccurence) {
+        if (cc == str.length()) {
+            for (char ch : spots) {
+                System.out.print(ch);
+            }
+            System.out.println();
+            return;
+        }
+        char ch = str.charAt(cc);
+        int lo = lastOccurence.get(ch);
+        for (int i = lo + 1; i < spots.length; i++) {
+            if (spots[i] == null) {
+                lastOccurence.put(ch, i);
+                spots[i] = ch;
+                generateWords(cc + 1, str, spots, lastOccurence);
+                lastOccurence.put(ch, lo);
+                spots[i] = null;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        Character[] spots = new Character[str.length()];
+        HashMap<Character, Integer> lastOccurence = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            lastOccurence.put(ch, -1);
+        }
+        generateWords(0, str, spots, lastOccurence);
+    }
 }
