b9ba488465a38b4125b720d36b8fcf4ed1b3cb4a
Eshan Bhatt <eshan18.09@gmail.com>
2021-02-28 16:01:28 +0530

    added more questions

--- Recursion and Backtracking/Words_K_Selection1.java
+++ Recursion and Backtracking/Words_K_Selection1.java
@@ -8,5 +8,33 @@ import java.io.*;
 import java.util.*;
 
 public class Words_K_Selection1 {
+    public static void generateSelection(int i, String ustr, int ssf, int ts, String asf) {
+        if (ssf == ts) {
+            System.out.println(asf);
+            return;
+        }
+        if (i == ustr.length()) {
+            return;
+        }
+        char ch = ustr.charAt(i);
+        generateSelection(i + 1, ustr, ssf + 1, ts, asf + ch);
+        generateSelection(i + 1, ustr, ssf, ts, asf);
+    }
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int k = Integer.parseInt(br.readLine());
+
+        HashSet<Character> unique = new HashSet<>();
+        String ustr = "";
+        for (char ch : str.toCharArray()) {
+            if (unique.contains(ch) == false) {
+                unique.add(ch);
+                ustr += ch;
+            }
+        }
+
+        generateSelection(0, ustr, 0, k, "");
+    }
 }
