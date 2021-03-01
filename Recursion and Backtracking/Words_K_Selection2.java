52ca95aa8487fce1fb325eeeb6a720d16759745a
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-01 16:15:59 +0530

    added more questions

--- Recursion and Backtracking/Words_K_Selection2.java
+++ Recursion and Backtracking/Words_K_Selection2.java
@@ -4,6 +4,36 @@
 3. You are required to generate and print all ways you can select k distinct characters out of the word.
 */
 
+import java.io.*;
+import java.util.*;
+
 public class Words_K_Selection2 {
 
+    public static void generateSelection(String ustr, int cs, int ts, int lc, String asf) {
+        if (cs > ts) {
+            System.out.println(asf);
+            return;
+        }
+        for (int i = lc + 1; i < ustr.length(); i++) {
+            char ch = ustr.charAt(i);
+            generateSelection(ustr, cs + 1, ts, i, asf + ch);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int k = Integer.parseInt(br.readLine());
+
+        HashSet<Character> unique = new HashSet<>();
+        String ustr = "";
+        for (char ch : str.toCharArray()) {
+            if (unique.contains(ch) == false) {
+                unique.add(ch);
+                ustr += ch;
+            }
+        }
+
+        generateSelection(ustr, 1, k, -1, "");
+    }
 }
