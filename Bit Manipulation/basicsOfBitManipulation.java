b20afdbdfe262b7218d39ab31de9648ceb89a681
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-08 16:18:11 +0530

    added more questions

--- Bit Manipulation/basicsOfBitManipulation.java
+++ Bit Manipulation/basicsOfBitManipulation.java
@@ -1,4 +1,11 @@
-import java.io.*;
+/*
+1. You are given a number n.
+2. Print the number produced on setting its i-th bit.
+3. Print the number produced on unsetting its j-th bit.
+4. Print the number produced on toggling its k-th bit.
+5. Also, Check if its m-th bit is on or off. Print 'true' if it is on, otherwise print 'false'.
+*/
+
 import java.util.*;
 
 public class basicsOfBitManipulation {
@@ -10,7 +17,6 @@ public class basicsOfBitManipulation {
         int k = scn.nextInt();
         int m = scn.nextInt();
 
-        // write your code here
         int onmask = (1 << i);
         int offmask = ~(1 << j);
         int togglemask = (1 << k);
