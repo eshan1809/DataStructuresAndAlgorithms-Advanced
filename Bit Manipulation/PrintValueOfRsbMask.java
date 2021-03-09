1ceaf5444566ac174b5f436463950625a340f09b
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-09 18:21:36 +0530

    added more questions

--- Bit Manipulation/PrintValueOfRsbMask.java
+++ Bit Manipulation/PrintValueOfRsbMask.java
@@ -3,9 +3,13 @@
 2. You have to print the right-most set bit mask.
 */
 
-import java.io.*;
 import java.util.*;
 
 public class PrintValueOfRsbMask {
-
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println(Integer.toBinaryString(n & -n));
+        scn.close();
+    }
 }
