a490c3363d94b3be80cd9f45eeac1097e6fa1224
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-11 20:51:36 +0530

    added more questions

---
+++ Bit Manipulation/KernighansAlgorithm.java
@@ -0,0 +1,11 @@
+/*
+1. You are given a number n.
+2. You have to count the number of set bits in the given number.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class KernighansAlgorithm {
+
+}
