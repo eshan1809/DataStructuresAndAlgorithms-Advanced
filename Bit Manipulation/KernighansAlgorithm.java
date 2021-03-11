5372f694208fb08567e701360d3988af3d9df594
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-11 20:52:03 +0530

    added more questions

--- Bit Manipulation/KernighansAlgorithm.java
+++ Bit Manipulation/KernighansAlgorithm.java
@@ -3,9 +3,19 @@
 2. You have to count the number of set bits in the given number.
 */
 
-import java.io.*;
 import java.util.*;
 
 public class KernighansAlgorithm {
-
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int cs = 0;
+        while (n != 0) {
+            int rsbm = (n & -n);
+            n -= rsbm;
+            cs++;
+        }
+        System.out.println(cs);
+        scn.close();
+    }
 }
