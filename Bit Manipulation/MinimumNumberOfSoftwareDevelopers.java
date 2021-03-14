eb3c4a8a0efdae6474913a2bd21e61ac79bb055d
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-14 07:58:08 +0530

    added more questions

---
+++ Bit Manipulation/MinimumNumberOfSoftwareDevelopers.java
@@ -0,0 +1,16 @@
+/*
+ * 1. You are given N strings which represents N different skills related to I.T
+ * field.
+ * 
+ * 2. You are working on a project and you want to hire a team of software
+ * developers for that project.
+ * 
+ * 3. There are N applicants. Every applicant has mentioned his/her skills in
+ * resume.
+ * 
+ * 4. You have to select the minimum number of developers such that for every
+ * required skill, there is at least one person in the team who has that skill.
+ * 
+ * 5. It is guaranteed that you can form a team which covers all the required
+ * skills.
+ */
