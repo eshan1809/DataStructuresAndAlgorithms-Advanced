d4a2ce1b902913094678eb0b81258e287dd31d30
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-21 12:16:37 +0530

    added more questions

---
+++ Recursion and Backtracking/Words_K_Selection4.java
@@ -0,0 +1,12 @@
+/*
+1. You are given a word (may have one character repeat more than once).
+2. You are given an integer k.
+3. You are required to generate and print all ways you can select k characters out of the word.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Words_K_Selection4 {
+
+}
