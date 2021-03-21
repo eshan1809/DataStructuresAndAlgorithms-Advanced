9c9ba65bf1b792d40bd40ceb878167fa0f985d76
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-21 12:17:22 +0530

    added more questions

--- Recursion and Backtracking/Words_K_Selection4.java
+++ Recursion and Backtracking/Words_K_Selection4.java
@@ -8,5 +8,39 @@ import java.io.*;
 import java.util.*;
 
 public class Words_K_Selection4 {
+    public static void generateSelection(int cs, int ts, String ustr, HashMap<Character, Integer> unique, int ls,
+            String asf) {
+        if (cs > ts) { // base case when we have occupied all cells
+            System.out.println(asf); // print ans
+            return;
+        }
 
+        for (int i = ls; i < ustr.length(); i++) { // we choose characters for this cell
+            char ch = ustr.charAt(i); // choose a char one by one
+            int f = unique.get(ch); // note down its frequency
+            if (f > 0) { // if it is atleast one
+                unique.put(ch, f - 1); // decrease by one
+                generateSelection(cs + 1, ts, ustr, unique, i, asf + ch); // call for next level
+                unique.put(ch, f); // undo while backtracking
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int k = Integer.parseInt(br.readLine());
+
+        HashMap<Character, Integer> unique = new HashMap<>();
+        String ustr = "";
+        for (char ch : str.toCharArray()) {
+            if (unique.containsKey(ch) == false) {
+                unique.put(ch, 1);
+                ustr += ch;
+            } else {
+                unique.put(ch, unique.get(ch) + 1);
+            }
+        }
+        generateSelection(1, k, ustr, unique, 0, "");
+    }
 }
