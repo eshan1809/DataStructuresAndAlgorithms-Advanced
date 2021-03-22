ae3e000b6c303cde5e637a4f5b87b9419c2fae12
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-22 18:45:12 +0530

    added more questions

---
+++ Recursion and Backtracking/Words_K_LengthWords3.java
@@ -0,0 +1,12 @@
+/*
+1. You are given a word (may have one character repeat more than once).
+2. You are given an integer k.
+3. You are required to generate and print all k length words by using chars of the word.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Words_K_LengthWords3 {
+
+}
