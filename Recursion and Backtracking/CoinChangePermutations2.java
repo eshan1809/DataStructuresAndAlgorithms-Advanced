bdfc2de4baefc9713750ad1e931dc7a4b3962da6
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-25 22:17:16 +0530

    added more questions

--- Recursion and Backtracking/CoinChangePermutations2.java
+++ Recursion and Backtracking/CoinChangePermutations2.java
@@ -6,8 +6,28 @@
 */
 
 import java.io.*;
-import java.util.*;
 
 public class CoinChangePermutations2 {
+    public static void coinChange(int[] coins, int amtsf, int tamt, String asf) {
+        if (amtsf >= tamt) { // if amount exceeds or no coin left
+            if (amtsf == tamt)
+                System.out.println(asf + "."); // if amount is equal to change
+            return;
+        }
 
+        for (int i = 0; i < coins.length; i++) { // we can choose any coin at this level
+            coinChange(coins, amtsf + coins[i], tamt, asf + coins[i] + "-"); // next level
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        int amt = Integer.parseInt(br.readLine());
+        coinChange(coins, 0, amt, "");
+    }
 }
