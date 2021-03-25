3e444a00d56c9e8de6b4407172482e72f806fe01
Eshan Bhatt <eshan18.09@gmail.com>
2021-03-25 22:16:39 +0530

    added more questions

---
+++ Recursion and Backtracking/CoinChangePermutations2.java
@@ -0,0 +1,13 @@
+/*
+1. You are given a number n, representing the count of coins.
+2. You are given n numbers, representing the denominations of n coins.
+3. You are given a number "amt".
+4. You are required to calculate and print the permutations of the n coins (same coin can be used again any number of times) using which the amount "amt" can be paid.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class CoinChangePermutations2 {
+
+}
