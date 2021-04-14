f192dec5144bba3a367d6a6b84ac13b5b17db38e
Eshan Bhatt <eshan18.09@gmail.com>
2021-04-14 14:01:12 +0530

    added more questions

--- Dynamic Programming/maximumSumIncreasingSubsequence.java
+++ Dynamic Programming/maximumSumIncreasingSubsequence.java
@@ -4,18 +4,24 @@ public class maximumSumIncreasingSubsequence {
     public static void main(String[] args) throws Exception {
         // write your code here
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt(), max = 1;
+        int n = scn.nextInt();
         int[] arr = new int[n], dp = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = scn.nextInt();
-        dp[0] = 1;
+        if (n == 0) {
+            System.out.println(0);
+            scn.close();
+            return;
+        }
+        dp[0] = arr[0];
+        int max = arr[0];
         for (int i = 1; i < n; i++) {
             int lMax = 0;
             for (int j = 0; j < i; j++)
-                if (arr[j] < arr[i])
+                if (arr[j] <= arr[i]) // arr[j] < arr[i] if required sequence is strictly increasing
                     lMax = Math.max(lMax, dp[j]);
-            dp[i] = lMax + 1;
-            max = Math.max(max, lMax + 1);
+            dp[i] = lMax + arr[i];
+            max = Math.max(max, dp[i]);
         }
         System.out.println(max);
         scn.close();
