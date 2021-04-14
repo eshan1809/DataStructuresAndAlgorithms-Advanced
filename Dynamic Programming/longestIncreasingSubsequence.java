dce6138e911183c7ddda12255f5d6050a5dc7284
Eshan Bhatt <eshan18.09@gmail.com>
2021-04-14 13:37:02 +0530

    Dynamic Programming

--- Dynamic Programming/longestIncreasingSubsequence.java
+++ Dynamic Programming/longestIncreasingSubsequence.java
@@ -4,18 +4,18 @@ public class longestIncreasingSubsequence {
     public static void main(String[] args) throws Exception {
         // write your code here
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt(), max = 1;
+        int n = scn.nextInt(), max = 0;
         int[] arr = new int[n], dp = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = scn.nextInt();
-        dp[0] = 1;
+        dp[0] = arr[0];
         for (int i = 1; i < n; i++) {
             int lMax = 0;
             for (int j = 0; j < i; j++)
                 if (arr[j] < arr[i])
                     lMax = Math.max(lMax, dp[j]);
-            dp[i] = lMax + 1;
-            max = Math.max(max, lMax + 1);
+            dp[i] = lMax + arr[i];
+            max = Math.max(max, dp[i]);
         }
         System.out.println(max);
         scn.close();
