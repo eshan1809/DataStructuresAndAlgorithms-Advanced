da86c9d01709fe9f4e026365021f4ccb167d5e40
Eshan Bhatt <eshan18.09@gmail.com>
2021-04-17 01:06:14 +0530

    added more questions

---
+++ Dynamic Programming/maximumNonoverlappingBridges.java
@@ -0,0 +1,60 @@
+/*
+1. You are given a number n, representing the number of bridges on a river.
+2. You are given n pair of numbers, representing the north bank and south bank co-ordinates of each bridge.
+3. You are required to print the count of maximum number of non-overlapping bridges.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class maximumNonoverlappingBridges {
+
+    static class Pair implements Comparable<Pair> {
+        int first, second; // first/second -> first/second end of bridge
+
+        Pair(int f, int s) {
+            first = f;
+            second = s;
+        }
+
+        public int compareTo(Pair o) {
+            if (this.first != o.first)
+                return this.first - o.first; // sorting on basis of first ends (increasingly)
+            else
+                return this.second - o.second;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        if (n == 0) {
+            System.out.println(0);
+            scn.close();
+            return;
+        }
+        Pair[] pairs = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            int f = scn.nextInt(), s = scn.nextInt();
+            pairs[i] = new Pair(f, s);
+        }
+        scn.close();
+        Arrays.sort(pairs);
+
+        // longest increasing subsequence
+        int[] dp = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int lmax = 0;
+            for (int j = 0; j < i; j++) {
+                if (pairs[i].second >= pairs[j].second) {
+                    lmax = Math.max(lmax, dp[j]);
+                }
+            }
+            dp[i] = lmax + 1;
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
---
+++ Dynamic Programming/russianDollEnvelopes.java
@@ -0,0 +1,59 @@
+/*
+1. You are given a number n, representing the number of envelopes.
+2. You are given n pair of numbers, representing the width and height of each envelope.
+3. You are required to print the count of maximum number of envelopes that can be nested inside each other.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class russianDollEnvelopes {
+    static class Pair implements Comparable<Pair> {
+        int first, second; // first/second -> first/second length, breadth of envelope
+
+        Pair(int f, int s) {
+            first = f;
+            second = s;
+        }
+
+        public int compareTo(Pair o) {
+            if (this.first != o.first)
+                return this.first - o.first; // sorting on basis of length (increasingly)
+            else
+                return this.second - o.second;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        if (n == 0) {
+            System.out.println(0);
+            scn.close();
+            return;
+        }
+        Pair[] pairs = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            int f = scn.nextInt(), s = scn.nextInt();
+            pairs[i] = new Pair(f, s);
+        }
+        scn.close();
+        Arrays.sort(pairs);
+
+        // longest increasing subsequence
+        int[] dp = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int lmax = 0;
+            for (int j = 0; j < i; j++) {
+                if (pairs[i].second > pairs[j].second && pairs[i].first > pairs[j].first) {
+                    lmax = Math.max(lmax, dp[j]);
+                }
+            }
+            dp[i] = lmax + 1;
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
