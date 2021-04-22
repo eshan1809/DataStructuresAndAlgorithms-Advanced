fa4f60a0d8acd31c557a306a33bbf0d80eb0135d
Eshan Bhatt <eshan18.09@gmail.com>
2021-04-23 00:02:58 +0530

    added more questions

--- Dynamic Programming/printAllResultsIn_0_1_Knapsack.java
+++ Dynamic Programming/printAllResultsIn_0_1_Knapsack.java
@@ -14,6 +14,17 @@ import java.io.*;
 import java.util.*;
 
 public class printAllResultsIn_0_1_Knapsack {
+    public static class Pair {
+        int i, j;
+        String psf;
+
+        Pair(int i, int j, String psf) {
+            this.i = i;
+            this.j = j;
+            this.psf = psf;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -33,6 +44,30 @@ public class printAllResultsIn_0_1_Knapsack {
         int cap = Integer.parseInt(br.readLine());
 
         // write your code here
+        int[][] dp = new int[n + 1][cap + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= cap; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (wts[i - 1] <= j)
+                    dp[i][j] = Math.max(dp[i][j], values[i - 1] + dp[i - 1][j - wts[i - 1]]);
+            }
+        }
+        System.out.println(dp[n][cap]);
 
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(n, cap, ""));
+
+        while (q.size() > 0) {
+            Pair p = q.remove();
+            if (p.i == 0 || p.j == 0)
+                System.out.println(p.psf);
+            else {
+                int x = p.j - wts[p.i - 1];
+                if (x >= 0 && dp[p.i][p.j] - values[p.i - 1] == dp[p.i - 1][x])
+                    q.add(new Pair(p.i - 1, p.j - wts[p.i - 1], (p.i - 1) + " " + p.psf));
+                if (dp[p.i][p.j] == dp[p.i - 1][p.j])
+                    q.add(new Pair(p.i - 1, p.j, p.psf));
+            }
+        }
     }
 }
