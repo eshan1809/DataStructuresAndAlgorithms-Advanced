062f8b193936aa79aa767cfb1602f6ce1ad7c557
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-14 03:23:32 +0530

    added more questions

---
+++ Dynamic Programming/editDistance.java
@@ -0,0 +1,40 @@
+/*
+1. You are given two strings s1 and s2.
+2. You have to find the minimum number of operations needed to convert s1 to s2.
+   Operations allowed are - 
+   Insert - You can insert any character in s1.
+   Remove - You can remove any character in s1.
+   Replace - You can replace any character in s1 with any other character.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class editDistance {
+    public static int solution(String str1, String str2) {
+        // write your code here
+        int n1 = str1.length(), n2 = str2.length();
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        for (int i = 0; i <= n1; i++)
+            dp[i][0] = i;
+        for (int i = 0; i <= n2; i++)
+            dp[0][i] = i;
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+            }
+        }
+        return dp[n1][n2];
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String s1 = scn.next();
+        String s2 = scn.next();
+        System.out.println(solution(s1, s2));
+    }
+}
