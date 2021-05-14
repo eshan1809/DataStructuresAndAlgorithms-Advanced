0e51957656e799a77189d01a7faf8266f71aff90
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-15 05:15:56 +0530

    added more questions

--- Dynamic Programming/maximumSumSubarrayWithAtLeast_K_Elements.java
+++ Dynamic Programming/maximumSumSubarrayWithAtLeast_K_Elements.java
@@ -10,8 +10,22 @@ import java.util.*;
 public class maximumSumSubarrayWithAtLeast_K_Elements {
     public static int solution(int[] arr, int k) {
         // write your code here
-
-        return 0;
+        int[] dp = new int[arr.length];
+        int curr = arr[0], sum = 0, ans;
+        dp[0] = curr;
+        for (int i = 1; i < arr.length; i++)
+            dp[i] = curr = Math.max(curr + arr[i], arr[i]);
+        for (int i = 0; i < k; i++)
+            sum += arr[i];
+        ans = sum;
+        for (int i = k; i < arr.length; i++) {
+            sum = sum + arr[i] - arr[i - k];
+            int val = sum;
+            if (dp[i - k] > 0)
+                val += dp[i - k];
+            ans = Math.max(ans, val);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
