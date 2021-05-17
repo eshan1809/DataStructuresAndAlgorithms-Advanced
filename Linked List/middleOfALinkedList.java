ed99ce9852bb15cc924cc0e0eb8c67c80cb025a6
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-18 02:54:12 +0530

    added more questions

---
+++ Linked List/middleOfALinkedList.java
@@ -0,0 +1,43 @@
+/*
+given a singly linked list with head node head, return a middle node of linked list.
+if there is 2 mid node then return first mid node.
+*/
+
+import java.util.*;
+
+public class middleOfALinkedList {
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode midNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+        scn.close();
+        ListNode head = midNode(dummy.next);
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
