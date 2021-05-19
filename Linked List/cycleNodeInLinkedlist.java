b5b5e3e81a5c28be86639f65ea40426b2511cb33
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-20 03:03:57 +0530

    added more questions

---
+++ Linked List/cycleNodeInLinkedlist.java
@@ -0,0 +1,58 @@
+/*
+1. Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+2. There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+
+Notice that you should not modify the linked list.
+*/
+
+import java.util.*;
+
+public class cycleNodeInLinkedlist {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode CycleNode(ListNode head) {
+        HashSet<ListNode> set = new HashSet();
+        ListNode temp = head;
+        while (temp != null) {
+            if (set.contains(temp))
+                return temp;
+            set.add(temp);
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public static ListNode takeInput() {
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+        int idx = scn.nextInt();
+        if (idx >= 0) {
+            ListNode curr = dummy.next;
+            while (idx-- > 0) {
+                curr = curr.next;
+            }
+            prev.next = curr;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = takeInput();
+        ListNode ans = CycleNode(head);
+        System.out.println(ans != null ? ans.val : -1);
+    }
+}
