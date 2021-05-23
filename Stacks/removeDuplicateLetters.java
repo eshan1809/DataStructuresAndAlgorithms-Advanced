1dfc40255e70a8685724457d44c869e043fb2080
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-23 16:59:25 +0530

    added more questions

---
+++ Stacks/removeDuplicateLetters.java
@@ -0,0 +1,21 @@
+/*
+1. Given a string s, remove duplicate letters so that every letter appears once and only once.
+2. You must make sure your result is the first in dictionary order among all possible results.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class removeDuplicateLetters {
+    public static String removeDuplicates(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        String result = removeDuplicates(read.readLine());
+        System.out.println(result);
+
+    }
+}
