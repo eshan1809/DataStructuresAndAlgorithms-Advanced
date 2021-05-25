5d05f3699ccc3d0e67d176ee29bc59006cd18760
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-26 01:41:53 +0530

    added more questions

---
+++ Stacks/basicCalculator.java
@@ -0,0 +1,21 @@
+/*
+Given a string s representing an expression, implement a basic calculator to evaluate it.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class basicCalculator {
+
+    public static int calculate(String s) {
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        int result = calculate(read.readLine());
+        System.out.println(result);
+
+    }
+}
