c585c384acca5415852b2e9e18017001e30a1567
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-26 01:51:46 +0530

    added more questions

--- Stacks/basicCalculator.java
+++ Stacks/basicCalculator.java
@@ -8,7 +8,32 @@ import java.util.*;
 public class basicCalculator {
 
     public static int calculate(String s) {
-        return 0;
+        Stack<Integer> st = new Stack<>();
+        int op = 0, ans = 0, sign = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '+') {
+                ans += sign * op;
+                sign = 1;
+                op = 0;
+            } else if (ch == '-') {
+                ans += sign * op;
+                sign = -1;
+                op = 0;
+            } else if (ch == '(') {
+                st.push(ans);
+                st.push(sign);
+                sign = 1;
+                ans = 0;
+            } else if (ch == ')') {
+                ans = (ans + sign * op) * st.pop() + st.pop();
+                op = 0;
+            } else if (ch == ' ')
+                continue;
+            else
+                op = op * 10 + (int) (ch - '0');
+        }
+        return ans + sign * op;
     }
 
     public static void main(String[] args) throws Exception {
