80d36b6eca198ae1a1741e9cb7087b1d6b65c2c6
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-27 15:53:26 +0530

    added more questions

--- Stacks/basicCalculator2.java
+++ Stacks/basicCalculator2.java
@@ -7,7 +7,25 @@ import java.io.*;
 
 public class basicCalculator2 {
     public static int calculate(String s) {
-
+        int n = s.length(), currNum = 0, prevNum = 0, ans = 0;
+        char op = '+';
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch))
+                currNum = currNum * 10 + (ch - '0');
+            if (!Character.isDigit(ch) && ch != ' ' || i == n - 1) {
+                if (op == '+' || op == '-') {
+                    ans += prevNum;
+                    prevNum = op == '+' ? currNum : -currNum;
+                } else if (op == '*')
+                    prevNum *= currNum;
+                else if (op == '/')
+                    prevNum /= currNum;
+                op = ch;
+                currNum = 0;
+            }
+        }
+        return ans + prevNum;
     }
 
     public static void main(String[] args) throws Exception {
