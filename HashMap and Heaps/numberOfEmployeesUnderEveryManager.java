bd264804e41764499a10a09b0c928c27d0e7a0ca
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-28 07:31:59 +0530

    added more questions

---
+++ HashMap and Heaps/numberOfEmployeesUnderEveryManager.java
@@ -0,0 +1,19 @@
+/*
+1. You are given number N and 2*N number of strings that contains mapping of the employee and his manager.
+2. An employee directly reports to only one manager. 
+3. All managers are employees but the reverse is not true.
+4. An employee reporting to himself is the CEO of the company.
+5. You have to find the number of employees under each manager in the hierarchy not just their direct reports.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class numberOfEmployeesUnderEveryManager {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        // write your code here
+    }
+}
