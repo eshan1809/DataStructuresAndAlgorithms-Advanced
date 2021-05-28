42761275def0b0320af499bea8ec3a0c4d57cf36
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-28 10:09:29 +0530

    added more questions

--- HashMap and Heaps/numberOfEmployeesUnderEveryManager.java
+++ HashMap and Heaps/numberOfEmployeesUnderEveryManager.java
@@ -6,14 +6,54 @@
 5. You have to find the number of employees under each manager in the hierarchy not just their direct reports.
 */
 
-import java.io.*;
 import java.util.*;
 
 public class numberOfEmployeesUnderEveryManager {
+    public static void solve(Map<String, String> map) {
+        Map<String, Set<String>> tree = new HashMap<>();
+        String ceo = "";
+        for (String emp : map.keySet()) {
+            String man = map.get(emp);
+            if (man.equals(emp))
+                ceo = man;
+            else {
+                if (tree.containsKey(man))
+                    tree.get(man).add(emp);
+                else {
+                    Set<String> set = new HashSet<>();
+                    set.add(emp);
+                    tree.put(man, set);
+                }
+            }
+        }
+        Map<String, Integer> ans = new HashMap<>();
+        int x = getAns(tree, ceo, ans);
+        for (String emp : ans.keySet())
+            System.out.println(emp + " " + ans.get(emp));
+
+    }
+
+    public static int getAns(Map<String, Set<String>> tree, String man, Map<String, Integer> ans) {
+        if (!tree.containsKey(man)) {
+            ans.put(man, 0);
+            return 1;
+        }
+        int res = 0;
+        for (String emp : tree.get(man))
+            res += getAns(tree, emp, ans);
+        ans.put(man, res);
+        return res + 1;
+    }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
+        int n = Integer.parseInt(scn.next());
 
         // write your code here
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            map.put(scn.next(), scn.next());
+        solve(map);
+        scn.close();
     }
 }
