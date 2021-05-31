80d4dd7fca6647c317a707d478326c0f6bea2128
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-31 21:58:42 +0530

    added more questions

---
+++ HashMap and Heaps/countOfSubstringsHavingAllUniqueCharacters.java
@@ -0,0 +1,22 @@
+/*
+1. You are given a string.
+2. You have to find the count of valid substrings of the given string.
+3. Valid substring is defined as a substring that has all unique characters.
+*/
+
+import java.util.*;
+
+public class countOfSubstringsHavingAllUniqueCharacters {
+    public static int solution(String str) {
+        // write your code here
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(solution(str));
+        scn.close();
+    }
+}
