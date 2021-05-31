779811a608e6917218e7dba6f251e705e17d1baa
Eshan Bhatt <eshan18.09@gmail.com>
2021-05-31 12:57:13 +0530

    added more questions

--- HashMap and Heaps/largestSubarrayWithContiguousElements.java
+++ HashMap and Heaps/largestSubarrayWithContiguousElements.java
@@ -10,8 +10,22 @@ import java.util.*;
 public class largestSubarrayWithContiguousElements {
     public static int solution(int[] arr) {
         // write your code here
-
-        return 0;
+        int ans = 1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = arr[i], max = arr[i];
+            Set<Integer> set = new HashSet<>();
+            set.add(arr[i]);
+            for (int j = i + 1; j < arr.length; j++) {
+                if (set.contains(arr[j]))
+                    break;
+                set.add(arr[j]);
+                min = Math.min(min, arr[j]);
+                max = Math.max(max, arr[j]);
+                if (max - min == j - i)
+                    ans = Math.max(ans, j - i + 1);
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
