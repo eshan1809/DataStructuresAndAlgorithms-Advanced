9fe51dd2d1a50a119b8c413982e4feec00a4c21c
Eshan Bhatt <eshan18.09@gmail.com>
2021-06-01 13:08:40 +0530

    added more questions

--- HashMap and Heaps/countOfSubstringsHavingAllUniqueCharacters.java
+++ HashMap and Heaps/countOfSubstringsHavingAllUniqueCharacters.java
@@ -9,8 +9,18 @@ import java.util.*;
 public class countOfSubstringsHavingAllUniqueCharacters {
     public static int solution(String str) {
         // write your code here
-
-        return 0;
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                if (set.contains(ch))
+                    break;
+                set.add(ch);
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
