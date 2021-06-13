e92958fce565004558b081b8d311e537b783cdea
Eshan Bhatt <eshan18.09@gmail.com>
2021-06-13 21:49:03 +0530

    added more questions

--- Trees/leftViewOfABinarytree.java
+++ Trees/leftViewOfABinarytree.java
@@ -19,8 +19,23 @@ public class leftViewOfABinarytree {
     }
 
     public static ArrayList<Integer> leftView(TreeNode root) {
-
-        return null;
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<TreeNode> q = new ArrayDeque<>();
+        if (root != null)
+            q.add(root);
+        while (q.size() > 0) {
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode t = q.remove();
+                if (i == 0)
+                    list.add(t.val);
+                if (t.left != null)
+                    q.add(t.left);
+                if (t.right != null)
+                    q.add(t.right);
+            }
+        }
+        return list;
     }
 
     // input_section=================================================
