26d45233c8fabb740e333553c1ac93ed0a4fec9c
Eshan Bhatt <eshan18.09@gmail.com>
2021-06-19 20:03:57 +0530

    added more questions

---
+++ Dynamic Programming/maximumSumOfThreeNonOverlappingSubarrays.java
@@ -0,0 +1,28 @@
+/*
+1. You are given an array(arr) of positive numbers and a number K.
+2. You have to find the maximum sum of elements in three non-overlapping subarrays.
+3. Also, you have to print indices representing the starting position of every subarray.
+4. If there are multiple answers, print the lexicographically smallest one.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class maximumSumOfThreeNonOverlappingSubarrays {
+    public static void solution(int[] arr, int k) {
+        // write your code here
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int k = scn.nextInt();
+        scn.close();
+        solution(arr, k);
+    }
+}
