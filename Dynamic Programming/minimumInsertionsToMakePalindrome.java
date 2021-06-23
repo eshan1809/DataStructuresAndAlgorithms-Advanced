e617a2d2894d38cf0295d16d2c55b8c80242c5ff
Eshan Bhatt <eshan18.09@gmail.com>
2021-06-23 16:10:10 +0530

    added more questions

---
+++ Dynamic Programming/minimumInsertionsToMakePalindrome.java
@@ -0,0 +1,31 @@
+/*
+1. You are given a string(str).
+2. You have to find the minimum number of characters to be inserted to convert it to a palindrome.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class minimumInsertionsToMakePalindrome {
+    public static int solution(String str) {
+        // write your code here
+        int n = str.length();
+        int[][] dp = new int[n][n];
+        for (int k = 1; k < n; k++) {
+            for (int i = 0, j = k; j < n; i++, j++) {
+                if (str.charAt(i) == str.charAt(j))
+                    dp[i][j] = dp[i + 1][j - 1];
+                else
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i + 1][j]) + 1;
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        scn.close();
+        System.out.println(solution(str));
+    }
+}
