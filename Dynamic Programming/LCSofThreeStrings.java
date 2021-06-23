badc3e8928ee18ebc6e45cf070cc885299e811d9
Eshan Bhatt <eshan18.09@gmail.com>
2021-06-23 21:05:28 +0530

    added more questions

---
+++ Dynamic Programming/LCSofThreeStrings.java
@@ -0,0 +1,24 @@
+/*
+1. You are given three strings s1, s2 and s3.
+2. You have to find the longest common sub-sequence in the given strings.
+*/
+
+import java.util.*;
+
+public class LCSofThreeStrings {
+
+    public static void solution(String str1, String str2, String str3) {
+        // write your code here
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str1 = scn.next();
+        String str2 = scn.next();
+        String str3 = scn.next();
+        scn.close();
+        solution(str1, str2, str3);
+    }
+
+}
