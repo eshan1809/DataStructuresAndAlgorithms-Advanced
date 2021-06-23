aeeca180113cce72b2bfd226a240d8360e1c9ffe
Eshan Bhatt <eshan18.09@gmail.com>
2021-06-23 21:05:34 +0530

    added more questions

--- Dynamic Programming/LCSofThreeStrings.java
+++ Dynamic Programming/LCSofThreeStrings.java
@@ -9,7 +9,18 @@ public class LCSofThreeStrings {
 
     public static void solution(String str1, String str2, String str3) {
         // write your code here
-
+        int[][][] dp = new int[str1.length() + 1][str2.length() + 1][str3.length() + 1];
+        for (int i = str1.length() - 1; i >= 0; i--) {
+            for (int j = str2.length() - 1; j >= 0; j--) {
+                for (int k = str3.length() - 1; k >= 0; k--) {
+                    if (str1.charAt(i) == str2.charAt(j) && str1.charAt(i) == str3.charAt(k))
+                        dp[i][j][k] = dp[i + 1][j + 1][k + 1] + 1;
+                    else
+                        dp[i][j][k] = Math.max(Math.max(dp[i + 1][j][k], dp[i][j + 1][k]), dp[i][j][k + 1]);
+                }
+            }
+        }
+        System.out.println(dp[0][0][0]);
     }
 
     public static void main(String[] args) {
