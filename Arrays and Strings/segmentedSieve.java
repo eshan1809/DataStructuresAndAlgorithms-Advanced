1800873b297a37fcf82ea0e59707946d26359afe
Eshan Bhatt <eshan18.09@gmail.com>
2021-06-26 11:33:49 +0530

    added more questions

--- Arrays and Strings/segmentedSieve.java
+++ Arrays and Strings/segmentedSieve.java
@@ -11,8 +11,7 @@ import java.util.*;
 public class segmentedSieve {
 
     // ~~~~~~~~~~~~~User Section~~~~~~~~~~~~
-
-    public static void segmentedSieveAlgo(int a, int b) {
+    public static ArrayList<Integer> sieve(int n) {
         // write your code here
         boolean[] isPrime = new boolean[n + 1];
         Arrays.fill(isPrime, true);
@@ -21,9 +20,29 @@ public class segmentedSieve {
                 for (int j = 2; j * i <= n; j++)
                     isPrime[i * j] = false;
         }
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 2; i <= n; i++)
             if (isPrime[i])
-                System.out.print(i + " ");
+                list.add(i);
+        return list;
+    }
+
+    public static void segmentedSieveAlgo(int a, int b) {
+        // write your code here
+        boolean[] arr = new boolean[b - a + 1];
+        Arrays.fill(arr, true);
+        ArrayList<Integer> primes = sieve((int) Math.sqrt(b));
+        for (int prime : primes) {
+            int multiple = (int) Math.ceil((double) a / prime);
+            if (multiple == 1)
+                multiple++;
+            int idx = multiple * prime - a;
+            for (int j = idx; j < arr.length; j += prime)
+                arr[j] = false;
+        }
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] && i + a != 1)
+                System.out.println(a + i);
     }
 
     // ~~~~~~~~~~~~Input Management~~~~~~~~~~~
@@ -31,6 +50,7 @@ public class segmentedSieve {
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
         int b = scn.nextInt();
+        scn.close();
         segmentedSieveAlgo(a, b);
     }
 }
