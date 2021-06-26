2ae331a319e97e5ac1690f90a7db869d80022dda
Eshan Bhatt <eshan18.09@gmail.com>
2021-06-26 16:17:43 +0530

    added more questions

---
+++ Arrays and Strings/wiggleSort1.java
@@ -0,0 +1,51 @@
+/*
+1. Given an unsorted array 'arr'.
+2. Reorder it in-place such that :  arr[0] <= arr[1] >= arr[2] <= arr[3] . . . .
+3. Please sort the array in place and do not define additional arrays.
+4. Allowed Time Complexity : O(n)
+*/
+
+import java.util.*;
+
+public class wiggleSort1 {
+
+    // ~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~
+    public static void wiggleSort(int[] arr) {
+        // write your code here
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (i % 2 == 0) {
+                if (arr[i + 1] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            } else {
+                if (arr[i + 1] > arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // ~~~~~~~~~~~~Input Management~~~~~~~~~~~~~
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        wiggleSort(arr);
+
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+}
