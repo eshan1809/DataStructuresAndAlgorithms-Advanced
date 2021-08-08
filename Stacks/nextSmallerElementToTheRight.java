c0d2468482dd05b38c38dfa778b5c2dd20259f40
Eshan Bhatt <eshan18.09@gmail.com>
2021-08-08 16:04:04 +0530

    added more questions

---
+++ Stacks/nextSmallerElementToTheRight.java
@@ -0,0 +1,53 @@
+/*
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing elements of array a.
+3. You are required to "next smaller element on the right" for all elements of array
+4. Input and output is handled for you.
+
+"Next smaller element on the right" of an element x is defined as the first element to right of x having value smaller than x.
+Note -> If an element does not have any element on it's right side smaller than it, consider -1 as it's "next smaller element on right"
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class nextSmallerElementToTheRight {
+    public static void display(int[] a) {
+        StringBuilder sb = new StringBuilder();
+        for (int val : a) {
+            sb.append(val + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] nge = solve(a);
+        display(nge);
+    }
+
+    public static int[] solve(int[] arr) {
+        // solve
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() > arr[i])
+                st.pop();
+            if (st.isEmpty())
+                ans[i] = -1;
+            else
+                ans[i] = st.peek();
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+
+}
