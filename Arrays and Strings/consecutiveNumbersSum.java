b7550d75f258d9365cf71bd173bee7d6386c8ce1
Eshan Bhatt <eshan18.09@gmail.com>
2021-08-31 23:16:35 +0530

    added more questions

--- Arrays and Strings/consecutiveNumbersSum.java
+++ Arrays and Strings/consecutiveNumbersSum.java
@@ -3,6 +3,28 @@
 2. Return the number of ways you can write n as the sum of consecutive positive integers.
 */
 
+import java.util.*;
+
 public class consecutiveNumbersSum {
 
+    // ~~~~~~~~~~~User's Section~~~~~~~~~~~~~
+    public static int solution(int n) {
+        // write your code here
+        int count = 0, i = 1;
+        while (n > 0) {
+            n -= i;
+            if (n % i == 0)
+                count++;
+            i++;
+        }
+        return count;
+    }
+
+    // ~~~~~~~~~Input Management~~~~~~~~~~~~~~
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        int res = solution(num);
+        System.out.println(res);
+    }
 }
